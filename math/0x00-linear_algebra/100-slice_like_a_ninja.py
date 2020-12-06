b0a9cf4a454f6e2a7a22ca9a6f5ae2e19e43375c
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-12-06 21:16:58 +0100

    100-slice_like_a_ninja.py

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -3,10 +3,11 @@
 
 
 def np_slice(matrix, axes={}):
-    start = list(axes.keys())[0]
+    """slices a matrix along specific axes"""
+    """start = list(axes.keys())[0]"""
     stop = max(list(axes.keys()))
     matrix_new = []
-    for i in range(start, stop + 1):
+    for i in range(0, stop + 1):
         if (i in list(axes.keys())):
             list1 = axes[i]
             matrix_new.append(slice(*list1))
