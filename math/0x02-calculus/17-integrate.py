ea9fabf668465ee43a2d9e05616cf200fe0b0e08
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-12-10 19:54:38 +0100

    integral

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""calculates the integral of a polynomial"""
+
+
+def poly_derivative(poly):
+    """calculates the integral of a polynomial"""
+    if isinstance(poly, list) == 0 or poly == []:
+        return None
+    if len(poly) == 1:
+        return [0]
+    new_poly = [0]
+    j = 0
+    for item in poly:
+        j = j + 1
+        x = item / j
+        if int(x) == x:
+            new_poly.append((int)(x))
+        else : 
+            new_poly.append(x)
+    return new_poly
