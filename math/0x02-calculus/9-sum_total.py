3df90588157bbe1a9f5c9328e7930100383fc24c
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-12-10 14:08:02 +0100

    sum sigma

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
+"""sigma au carré"""
+
+
 def summation_i_squared(n):
+    """return sum sigma au carré"""
+    if n is None or n <= 0:
+        return None
     return (int)(n * (n + 1) * (2 * n + 1) / 6)
