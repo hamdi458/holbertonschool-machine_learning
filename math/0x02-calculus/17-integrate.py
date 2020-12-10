e47267a880646de13bc7e59a1a546b08a7831d1b
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-12-10 20:00:06 +0100

    wiw

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -2,13 +2,13 @@
 """calculates the integral of a polynomial"""
 
 
-def poly_derivative(poly, c=0):
+def poly_derivative(poly, C=0):
     """calculates the integral of a polynomial"""
     if isinstance(poly, list) == 0 or poly == []:
         return None
     if len(poly) == 1:
         return [0]
-    new_poly = [c]
+    new_poly = [C]
     j = 0
     for item in poly:
         j = j + 1
