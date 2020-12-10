e39f6c0ff223db8f7c1de8a7c01adc19c778d729
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-12-10 15:01:44 +0100

    polynominale

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""calculates the derivative of a polynomial"""
+
+
+def poly_derivative(poly):
+    """calculates the derivative of a polynomial"""
+    if isinstance(poly, list) == 0:
+        return None
+    if len(poly) == 1:
+        return [0]
+    poly.pop(0)
+    new_poly = []
+    j = 0
+    for i in range(0, len(poly)):
+        new_poly.append(poly[i] * (j + 1))
+        j = j + 1
+    return new_poly
