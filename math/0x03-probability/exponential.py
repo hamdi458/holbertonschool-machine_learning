438ca8c59395cd1b4521ea43815a59f5953f9576
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-12-15 12:27:41 +0100

    exponential

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -3,12 +3,11 @@
 
 
 class Exponential:
-    """class poisson"""
-
+    """class exponential"""
     def __init__(self, data=None, lambtha=1.):
         self.data = data
         if data is None:
-            if lambtha < 0:
+            if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = (float)(lambtha)
         else:
