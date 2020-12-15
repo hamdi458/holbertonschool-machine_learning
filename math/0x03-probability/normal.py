b33ea5ef89258deea7ab848a38dd95e0ece33e67
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-12-15 12:41:22 +0100

    prob

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -10,18 +10,18 @@ class Normal:
         if data is None:
             if stddev <= 0:
                 raise ValueError("stddev must be a positive value")
-            self.mean = (float)(mean)
-            self.stddev = (float)(stddev)
+            self.stddev = float(stddev)
+            self.mean = float(mean)
         else:
-            if type(data) is not list:
-                raise ValueError("data must be a list")
-            if len(data) <= 2:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 3:
                 raise ValueError("data must contain multiple values")
-            self.mean = sum(data) / len(data)
-            std = 0
-            for item in data:
-                std += (item - self.mean) ** 2
-            self.stddev = (std / len(data)) ** 0.5
+            self.mean = sum(data)/len(data)
+            s = 0
+            for i in data:
+                s += (i - self.mean) ** 2
+            self.stddev = (s/len(data)) ** 0.5
 
     def z_score(self, x):
         """calcul pdf"""
