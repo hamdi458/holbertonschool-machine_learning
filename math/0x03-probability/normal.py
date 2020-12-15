e07a5bb9f6f2f061f67cb96b3e9c5abe25a0ba5c
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-12-15 12:38:41 +0100

    prob

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -9,14 +9,14 @@ class Normal:
         if data is None:
             self.mean = (float)(mean)
             self.stddev = (float)(stddev)
-            if stddev < 0:
+            if stddev <= 0:
                 raise ValueError("stddev must be a positive value")
         else:
-            self.mean = sum(data) / len(data)
             if type(data) is not list:
                 raise ValueError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
+            self.mean = sum(data) / len(data)
             std = 0
             for item in data:
                 std += (item - self.mean) ** 2
