efe32c4b9b71f0bb4f4af5e6bec696f379437d30
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-03 20:26:39 +0100

    task8

---
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -0,0 +1,22 @@
+ #!/usr/bin/env python3
+"""defines a single neuron performing binary classification"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """class NeuralNetwork"""
+    def __init__(self, nx, nodes):
+        if type(nx) is not int:
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be a integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.W1 = np.random.randn(1, nx)
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nx)
+        self.b2 = 0
+        self.A2 = 0
