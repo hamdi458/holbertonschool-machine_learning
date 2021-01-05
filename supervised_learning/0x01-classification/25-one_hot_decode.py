87f3d01f73990f1ec31c5fbf999f381db5a8f8d3
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-06 00:42:48 +0100

    one_hot_decode

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -5,6 +5,6 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     """converts a numeric label vector into a one-hot matrix"""
-    if len(one_hot.shape) == 2:
+    if len(one_hot.shape) == 2 and type(one_hot) == np.ndarray:
         return np.argmax(one_hot.T, axis=1)
     return None
