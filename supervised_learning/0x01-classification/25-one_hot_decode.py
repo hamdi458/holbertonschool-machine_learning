aea9dc2c58a49c16d78ed69d275e38c8ddf7846b
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-06 00:40:39 +0100

    one_hot_decode

---
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" one hot encode """
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """converts a numeric label vector into a one-hot matrix"""
+    if len(one_hot.shape) == 2:
+        return np.argmax(one_hot.T, axis=1)
+    return None
+    
