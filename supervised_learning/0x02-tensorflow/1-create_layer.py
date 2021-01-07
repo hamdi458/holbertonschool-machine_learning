96be32cced841913f5f1c37ec59584f5953b76e9
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-07 18:33:36 +0100

    task1

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -3,7 +3,8 @@
 
 import tensorflow as tf
 
+
 def create_layer(prev, n, activation):
     """the tensor output of the layer"""
-    initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    initialize = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     layer = tf.keras.layers.Dense(3, kernel_initializer=initializer)
