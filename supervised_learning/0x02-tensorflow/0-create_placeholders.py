f94d22b4bda4c1e8a750f651160b786a896f5f8d
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-07 18:11:36 +0100

    placeholder

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -6,5 +6,5 @@ import tensorflow as tf
 def create_placeholders(nx, classes):
     """eturns two placeholders, x and y, for the neural network"""
     x = tf.placeholder("float", name="x")
-    y = x * 2
+    y = tf.placeholder(x * 2, name="y")
     return x, y
