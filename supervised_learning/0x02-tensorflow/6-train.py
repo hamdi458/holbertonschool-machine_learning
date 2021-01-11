b6f41507b18e0cf0a78362b060fb6ac8d38f88c0
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-11 01:11:51 +0100

    fix

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -17,13 +17,14 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
     y_pred = forward_prop(x, layer_sizes, activations)
     loss = calculate_loss(y, y_pred)
-
+    accuracy = calculate_accuracy(y, y_pred)
+    train_op = create_train_op(loss, alpha)
     tf.add_to_collection('x', x)
     tf.add_to_collection('y', y)
     tf.add_to_collection('y_pred', y_pred)
     tf.add_to_collection('loss', loss)
-    tf.add_to_collection('accuracy', calculate_accuracy(y, y_pred))
-    tf.add_to_collection('train_op', create_train_op(loss, alpha))
+    tf.add_to_collection('accuracy', accuracy)
+    tf.add_to_collection('train_op', train_op)
     init = tf.global_variables_initializer()
     saver = tf.train.Saver()
     with tf.Session() as sess:
