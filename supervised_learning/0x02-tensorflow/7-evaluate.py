c86719a8455ce4ca588e03c0945a6017d55d8f27
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-11 01:50:42 +0100

    evaluate

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -16,9 +16,9 @@ def evaluate(X, Y, save_path):
         y = tf.get_collection('y', scope=None)[0]
         y_pred = tf.get_collection('y_pred', scope=None)[0]
         loss = tf.get_collection('loss', scope=None)[0]
-        acc = tf.get_collection('accuracy', scope=None)[0]
+        accuaracy = tf.get_collection('accuracy', scope=None)[0]
         train_op = tf.get_collection('train_op', scope=None)[0]
-        accuracy = sess.run(acc, {x: X, y: Y})
+        accuracy = sess.run(accuracy, {x: X, y: Y})
         j = sess.run(loss, {x: X, y: Y})
         y_pred = sess.run(y_pred, {x: X, y: Y})
 
