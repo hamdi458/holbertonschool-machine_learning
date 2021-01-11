4e396146b8d081dc79cbbd151f7f6f4671e1f72d
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-11 01:29:18 +0100

    fix

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -17,18 +17,24 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
     y_pred = forward_prop(x, layer_sizes, activations)
     loss = calculate_loss(y, y_pred)
-    accuracy = calculate_accuracy(y, y_pred)
-    train_op = create_train_op(loss, alpha)
+
     tf.add_to_collection('x', x)
     tf.add_to_collection('y', y)
     tf.add_to_collection('y_pred', forward_prop(x, layer_sizes, activations))
+
     tf.add_to_collection('loss', loss)
-    tf.add_to_collection('accuracy', accuracy)
-    tf.add_to_collection('train_op', train_op)
+    tf.add_to_collection('accuracy', calculate_accuracy(y, y_pred))
+    tf.add_to_collection('train_op', create_train_op(loss, alpha))
     init = tf.global_variables_initializer()
     saver = tf.train.Saver()
     with tf.Session() as sess:
         sess.run(init)
+        x = tf.get_collection('x', scope=None)[0]
+        y = tf.get_collection('y', scope=None)[0]
+        y_pred = tf.get_collection('y_pred', scope=None)[0]
+        loss = tf.get_collection('loss', scope=None)[0]
+        accuracy = tf.get_collection('accuracy', scope=None)[0]
+        train_op = tf.get_collection('train_op', scope=None)[0]
         for i in range(iterations + 1):
             j = sess.run(loss, {x: X_train, y: Y_train})
             acc = sess.run(accuracy, {x: X_train, y: Y_train})
