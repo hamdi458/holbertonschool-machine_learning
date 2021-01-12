10352008b905b09bf64548112aa1c65317fa7a26
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-12 18:44:21 +0100

    momentum

---
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" creates the training operation for a neural network in tensorflow
+using the gradient descent with momentum optimization algorithm """
+import tensorflow as tf
+
+
+def create_momentum_op(loss, alpha, beta1):
+    """ creates the training operation for a neural network in tensorflow
+    using the gradient descent with momentum optimization algorithm"""
+    op_n_n = tf.train.MomentumOptimizer(alpha, beta1)
+    return = op_n_n.minimize(loss)
