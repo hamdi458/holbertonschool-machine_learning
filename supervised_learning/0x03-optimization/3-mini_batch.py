76e501f231aaf7c8df7e2ce3ca96d8e7534f1fa1
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-12 18:35:01 +0100

    fix

---
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+import numpy as np
+shuffle_data = __import__('2-shuffle_data').shuffle_data
+
+
+def cat(num, datax, datay, batch_size):
+    """split data"""
+    arrdatax = []
+    arrdatay = []
+    for i in(range(num+1)):
+        arrdatax.append(datax[i*batch_size:(i+1)*batch_size])
+        arrdatay.append(datay[i*batch_size:(i+1)*batch_size])
+    return arrdatay, arrdatax
+
+
+def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
+                     batch_size=32, epochs=5,
+                     load_path="/tmp/model.ckpt",
+                     save_path="/tmp/model.ckpt"):
+
+    init = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init)
+        saver = tf.train.import_meta_graph('{}.meta'.format(load_path))
+        saver.restore(sess, '{}'.format(load_path))
+
+        x = tf.get_collection('x', scope=None)[0]
+        y = tf.get_collection('y', scope=None)[0]
+        ay_pred = tf.get_collection('y_pred', scope=None)[0]
+        loss = tf.get_collection('loss', scope=None)[0]
+        accuracy = tf.get_collection('accuracy', scope=None)[0]
+        train_op = tf.get_collection('train_op', scope=None)[0]
+
+        nbr_batches = X_train.shape[0]/batch_size
+        nbr_batches = int(nbr_batches)
+        for epoche in range(epochs+1):
+            X_shuffled_train, Y_shuffled_train = shuffle_data(X_train, Y_train)
+
+            train_loss = sess.run(loss, {
+                x: X_shuffled_train, y: Y_shuffled_train})
+            train_acc = sess.run(accuracy, {
+                x: X_shuffled_train, y: Y_shuffled_train})
+            valid_acc = sess.run(accuracy, {
+                x: X_valid, y: Y_valid})
+
+            valid_loss = sess.run(loss, {x: X_valid, y: Y_valid})
+
+            print("After {} epochs:".format(epoche))
+            print("\tTraining Cost: {}".format(train_loss))
+            print("\tTraining Accuracy: {}".format(train_acc))
+            print("\tValidation Cost: {}".format(valid_loss))
+            print("\tValidation Accuracy: {}".format(
+                valid_acc))
+            arrx, arry = cat(nbr_batches,
+                             Y_shuffled_train, X_shuffled_train, batch_size)
+
+            for i in range(nbr_batches+1):
+
+                train_loss = sess.run(loss, {x: arrx[i-1], y: arry[i-1]})
+                train_acc = sess.run(accuracy, {x: arrx[i-1], y: arry[i-1]})
+                if(i % 100 == 0 and i > 0):
+                    print("\tStep {}:".format(i))
+                    print("\tTraining Cost: {}".format(train_loss))
+                    print("\tTraining Accuracy: {}".format(train_acc))
+                sess.run(train_op, {x: arrx[i], y: arry[i]})
+        return saver.save(sess, save_path)
