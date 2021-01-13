035cac2687219b4b6c767dcc2331ec8f5eb5b150
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-13 21:52:43 +0100

    fix

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -62,7 +62,7 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
             if epoche != epochs:
                 arrx, arry = cat(Y_shuffled_train, X_shuffled_train, batch_size)
                 for i in range(1, len(arrx)+1):
-                    sess.run(train_op, {x: arrx[i], y: arry[i]})
+                    sess.run(train_op, {x: arrx[i-1], y: arry[i-1]})
                     train_loss = sess.run(loss, {x: arrx[i-1], y: arry[i-1]})
                     train_acc = sess.run(accuracy, {x: arrx[i-1], y: arry[i-1]})
                     if(i % 100 == 0 and i > 0):
