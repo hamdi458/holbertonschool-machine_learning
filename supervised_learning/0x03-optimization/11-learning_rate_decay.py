ced62e07c1d952fff2cdda5f6911a493b92deaae
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-13 17:59:56 +0100

    learning rate decay

--- supervised_learning/0x03-optimization/11-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/11-learning_rate_decay.py
@@ -6,5 +6,5 @@ that updates the learning rate using inverse time decay in numpy:"""
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """updates the learning rate using inverse time decay in numpy"""
-    alpha = alpha / (1 + (decay_rate * global_step // decay_step))
-    return alpha
+    return alpha / (1 + (decay_rate * global_step // decay_step))
+
