f762f953ecee16e1526fb82c22cffc5a6abceb14
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-13 18:33:30 +0100

    learning rate decay operation

--- supervised_learning/0x03-optimization/12-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -5,13 +5,13 @@ using inverse time decay:"""
 import tensorflow as tf
 
 
-def batch_norm(Z, gamma, beta, epsilon):
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """creates a learning rate decay operation in tensorflow
     using inverse time decay"""
-    t = tf.compat.train.inverse_time_decay(
+    t = tf.train.inverse_time_decay(
         alpha,
         global_step,
-        decay_steps,
+        decay_step,
         decay_rate,
         staircase=True)
     return t
