2939687400e2913969d736aa0bc7d0e981d14d2c
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-18 15:56:39 +0100

    specifity

--- supervised_learning/0x04-error_analysis/3-specificity.py
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -2,11 +2,13 @@
 """function def specificity(confusion):
 that calculates the specificity for each class in a confusion matrix:"""
 
+import numpy as np
+
 
 def specificity(confusion):
     """calculates the specificity for each class in a confusion matrix"""
     x = 0
-    spec = []
+    spec = np.zeros(confusion.shape[0])
     for x in range(confusion.shape[0]):
         ffp = 0
         ttn = 0
@@ -16,5 +18,5 @@ def specificity(confusion):
                     ttn += confusion[i, j]
                 elif x == j:
                     ffp += confusion[i, j]
-        spec.append(ttn/(ttn+ffp-confusion[x, x]))
+        spec[x] += (ttn/(ttn+ffp-confusion[x, x]))
     return spec
