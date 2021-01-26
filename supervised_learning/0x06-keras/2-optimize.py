28ad2d21501d1ddd5d1ecf1a25c5bf47ee5bbf03
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-26 10:56:10 +0100

    fix

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
+"""function def optimize_model(network, alpha, beta1, beta2):
+that sets up Adam optimization for a keras model with categorical
+crossentropy loss and accuracy metrics:"""
 
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import *
+import tensorflow.keras as k
 
 
 def optimize_model(network, alpha, beta1, beta2):
+    """Adam optimization for a keras model with categorical"""
     opt = keras.optimizers.Adam(alpha, beta1, beta2)
     network.compile(loss='categorical_crossentropy', optimizer=opt,
                     metrics=['accuracy'])
