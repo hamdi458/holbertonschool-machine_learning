c05075e2e410b974a448e966de1649bbe99f700d
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-26 10:49:44 +0100

    keras

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -2,19 +2,19 @@
 """function def build_model(nx, layers, activations, lambtha, keep_prob):
 that builds a neural network with the Keras library:"""
 
-import tensorflow.keras as tf
+import tensorflow.keras as k
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    x = tf.Input(shape=(nx,))
+    x = k.Input(shape=(nx,))
     for i in range(len(layers)):
         if i == 0:
-            y = (tf.layers.Dense(layers[i], activation=activations[i],
-                 kernel_regularizer=tf.regularizers.l2(lambtha)))(x)
+            y = (k.layers.Dense(layers[i], activation=activations[i],
+                 kernel_regularizer=k.regularizers.l2(lambtha)))(x)
         else:
-            y = (tf.layers.Dense(layers[i], activation=activations[i],
-                 kernel_regularizer=tf.regularizers.l2(lambtha)))(y)
-        if i != len(layers) - 1:
-            y = (tf.layers.Dropout(1-keep_prob))(y)
-    model = tf.Model(x, y)
+            y = (k.layers.Dense(layers[i], activation=activations[i],
+                 kernel_regularizer=k.regularizers.l2(lambtha)))(y)
+        if i < len(layers) - 1:
+            y = (k.layers.Dropout(1-keep_prob))(y)
+    model = k.Model(x, y)
     return model
