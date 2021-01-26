c64ae5521282e67ad7fcdcf360aef0f5cba842c5
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-26 16:49:25 +0100

    weights

--- supervised_learning/0x06-keras/10-weights.py
+++ supervised_learning/0x06-keras/10-weights.py
@@ -4,13 +4,13 @@
 import tensorflow.keras as K
 
 
-def save_weights(network, filename):
+def save_weights(network, filename, save_format='h5'):
     """saves an weights"""
     network.save_weights(filename, save_format=save_format)
     return None
 
 
-def load_weights(filename):
+def load_weights(network, filename):
     """loads an weights"""
     network.load_weights(filename)
     return None
