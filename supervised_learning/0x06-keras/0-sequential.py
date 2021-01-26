bfe0bf8316e3f6a1a40d26ed82d3d63d2d824bb4
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-26 10:27:18 +0100

    keras

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -2,23 +2,19 @@
 """function def build_model(nx, layers, activations, lambtha, keep_prob):
 that builds a neural network with the Keras library"""
 
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import *
-
-from tensorflow.keras.layers import *
+import tensorflow.keras as tf
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """builds a neural network with the Keras library"""
-    model = keras.Sequential()
+    model = tf.Sequential()
     for i in range(len(layers)):
         if i == 0:
-            model.add(Dense(layers[i], input_dim=nx, activation=activations[i],
-                      kernel_regularizer=tf.keras.regularizers.l2(lambtha)))
-        else:
-            model.add(Dense(layers[i], activation=activations[i],
-                      kernel_regularizer=tf.keras.regularizers.l2(lambtha)))
+            model.add(tf.layers.Dense(layers[i], input_dim=nx, activation=activations[i],
+                      kernel_regularizer=tf.regularizers.l2(lambtha)))
+        if i > 0:
+            model.add(tf.layers.Dense(layers[i], activation=activations[i],
+                      kernel_regularizer=tf.regularizers.l2(lambtha)))
         if i < len(layers) - 1:
-            model.add(Dropout(1-keep_prob))
+            model.add(tf.layers.Dropout(1-keep_prob))
     return model
