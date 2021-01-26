43987ede03d531f8437cef55401961afdb903f62
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-26 11:32:30 +0100

    Early Stopping

--- supervised_learning/0x06-keras/6-train.py
+++ supervised_learning/0x06-keras/6-train.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import *
+""" train the model using early stopping"""
+import tensorflow.keras as k
 
-def train_model(network, data, labels, batch_size, epochs, validation_data=None, early_stopping=False, patience=0, verbose=True, shuffle=False):
-    es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', verbose=verbose, patience=patience)
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False,
+                patience=0, verbose=True, shuffle=False):
+    """ train the model using early stopping:"""
+    es = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
+                                          mode='min', verbose=verbose,
+                                          patience=patience)
     if early_stopping and validation_data:
-        history = network.fit(data, labels, batch_size=batch_size, epochs=epochs, verbose=verbose, shuffle=shuffle, validation_data=validation_data, callbacks=[es])
+        history = network.fit(data, labels, batch_size=batch_size,
+                              epochs=epochs, verbose=verbose,
+                              shuffle=shuffle, validation_data=validation_data,
+                              callbacks=[es])
     else:
-        history = network.fit(data, labels, batch_size=batch_size, epochs=epochs, verbose=verbose, shuffle=shuffle, validation_data=validation_data)
+        history = network.fit(data, labels, batch_size=batch_size,
+                              epochs=epochs, verbose=verbose, shuffle=shuffle,
+                              validation_data=validation_data)
 
     return history
