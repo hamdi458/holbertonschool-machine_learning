c5b039604f5b4b9b9f97956eee858de7fbca2698
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-26 17:11:50 +0100

    Save and Load configuaration

--- supervised_learning/0x06-keras/11-config.py
+++ supervised_learning/0x06-keras/11-config.py
@@ -4,7 +4,7 @@ import tensorflow.keras as K
 
 
 def save_config(network, filename):
-    """ saves a model’s weights:"""
+    """saves a model’s weights:"""
     with open(filename, "w") as fi:
         fi.write(network.to_json())
     return None
@@ -13,6 +13,6 @@ def save_config(network, filename):
 def load_config(filename):
     """loads a model’s weights"""
     with open(filename, "r") as fi:
-        r = fi.read()
-    load = k.models.model_from_json(r)
+        read = fi.read()
+    load = k.models.model_from_json(read)
     return load
