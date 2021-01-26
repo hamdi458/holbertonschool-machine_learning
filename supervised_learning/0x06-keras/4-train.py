2b1c36301960a5f14998a73e2330ca6291c70d60
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-26 11:27:22 +0100

    train

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -6,6 +6,6 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size,
                 epochs, verbose=True, shuffle=False):
     """that trains a model using mini-batch gradient descent"""
-    history = network.fit(x=data, y=labels, batch_size=batch_size,
+    history = network.fit(data, labels, batch_size=batch_size,
                           epochs=epochs, verbose=verbose, shuffle=shuffle)
     return history
--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
+"""analyze validaiton data"""
+import tensorflow.keras as k
 
-def train_model(network, data, labels, batch_size, epochs, validation_data=None, verbose=True, shuffle=False):
-    history = network.fit(data, labels, batch_size=batch_size, epochs=epochs, verbose=verbose, shuffle=shuffle, validation_data=validation_data)
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """train"""
+    history = network.fit(data, labels, batch_size=batch_size,
+                          epochs=epochs, verbose=verbose, shuffle=shuffle,
+                          validation_data=validation_data)
     return history
