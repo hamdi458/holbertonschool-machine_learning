456c830421b2f7946ff26a2bfa406242374e81ef
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-01-27 17:23:48 +0100

    Convolution with Padding

---
+++ math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""function def convolve_grayscale_same(images, kernel):
+that performs a same convolution on grayscale images:"""
+import numpy as np
+from math import ceil, floor
+
+
+def convolve_grayscale_padding(images, kernel, padding):
+    """images is a numpy.ndarray with shape (m, h, w) containing multiple
+    grayscale images
+            m is the number of images
+            h is the height in pixels of the images
+            w is the width in pixels of the images
+    kernel is a numpy.ndarray with shape (kh, kw) containing the kernel
+        for the convolution
+            kh is the height of the kernel
+            kw is the width of the kernel
+            Returns: a numpy.ndarray containing the convolved images"""
+    m, h, w = images.shape
+    mc = 0
+    Kr, Kc = kernel.shape
+    ph, pw = padding
+    image_padded = np.pad(images, ((0, 0), (ph-1, ph-1), (pw-1, pw-1)))
+    kr = Kr // 2
+    kc = Kc // 2
+    for dr in range(-kr, kr+1, 1):
+        mr = np.roll(image_padded, -dr, axis=1)
+        for dc in range(-kc, kc + 1, 1):
+            mrc = np.roll(mr, -dc, axis=2)
+            mc = mc+kernel[dr+kr, dc+kc]*mrc
+    return mc
