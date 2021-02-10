142577a3de0f7cfeb99a3de6d1862bda920974f2
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-02-10 16:28:28 +0100

    fix avgpool

--- supervised_learning/0x08-deep_cnns/4-resnet50.py
+++ supervised_learning/0x08-deep_cnns/4-resnet50.py
@@ -16,7 +16,7 @@ def resnet50():
     output = K.layers.BatchNormalization()(output)
     output = K.layers.Activation('relu')(output)
     output = K.layers.MaxPooling2D((3, 3), strides=(2, 2))(output)
-    output = projection_block(output, [64, 64, 256], s=1)
+    output = projection_block(output, [64, 64, 256])
     output = identity_block(output, [64, 64, 256])
     output = identity_block(output, [64, 64, 256])
     output = projection_block(output, [128, 128, 512])
