bdc44db38b06f09d896b687895b756ed0e8bd5bc
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-02-11 21:50:58 +0100

    fix req

--- supervised_learning/0x08-deep_cnns/7-densenet121.py
+++ supervised_learning/0x08-deep_cnns/7-densenet121.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""DenseNet-121
-builds the DenseNet-121 architecture"""
+"""DenseNet-121 builds the DenseNet-121 architecture"""
 import tensorflow.keras as K
 dense_block = __import__('5-dense_block').dense_block
 transition_layer = __import__('6-transition_layer').transition_layer
@@ -19,15 +18,15 @@ def densenet121(growth_rate=32, compression=1.0):
         transition_layer = __import__('6-transition_layer').transition_layer
     Returns: the keras model
     """
-    img_input = K.Input(shape=[224, 224, 3])
     kernel = K.initializers.he_normal(seed=None)
+    img_input = K.Input(shape=[224, 224, 3])
     X = K.layers.BatchNormalization()(img_input)
     X = K.layers.ReLU()(X)
     filters = 0
     if filters <= 0:
-        filters = 2*growth_rate
-    X = K.layers.Conv2D(filters,
-                        (7, 7), strides=2, padding="same",
+        filters = 2 * growth_rate
+    X = K.layers.Conv2D(filters=filters,
+                        kernel_size=(7, 7), strides=2, padding="same",
                         kernel_initializer=kernel)(X)
     X = K.layers.MaxPooling2D(pool_size=(3, 3), strides=2, padding='same')(X)
     X, filters = dense_block(X, filters, growth_rate, 6)
