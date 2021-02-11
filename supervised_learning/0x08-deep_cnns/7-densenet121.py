3d4852b2bd88ad35ff63b1c9fcb8c6bec5722377
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-02-11 22:07:29 +0100

    fix req

--- supervised_learning/0x08-deep_cnns/7-densenet121.py
+++ supervised_learning/0x08-deep_cnns/7-densenet121.py
@@ -15,27 +15,28 @@ def densenet121(growth_rate=32, compression=1.0):
     All weights should use he normal initialization
     Returns: the keras model
     """
+    X_input = K.Input(shape=(224, 224, 3))
     kernel = K.initializers.he_normal()
-    img_input = K.Input(shape=(224, 224, 3))
 
-    X = K.layers.BatchNormalization()(img_input)
-    X = K.layers.ReLU()(X)
+    X = K.layers.BatchNormalization(axis=3)(X_input)
+    X = K.layers.Activation('relu')(X)
     filters = 0
     if filters <= 0:
         filters = 2 * growth_rate
-    X = K.layers.Conv2D(filters=filters,
-                        kernel_size=(7, 7), strides=2, padding="same",
+    X = K.layers.Conv2D(filters, (7, 7), strides=(2, 2),
                         kernel_initializer=kernel)(X)
-    X = K.layers.MaxPooling2D(pool_size=(3, 3), strides=2, padding='same')(X)
-    X, filters = dense_block(X, filters, growth_rate, 6)
+
+    X = K.layers.MaxPooling2D(3, strides=2)(X)
+
+    X, filters = dense_block(X, nb_filters=filters,
+                             growth_rate=growth_rate, layers=6)
     X, filters = transition_layer(X, filters, compression)
     X, filters = dense_block(X, filters, growth_rate, 12)
     X, filters = transition_layer(X, filters, compression)
     X, filters = dense_block(X, filters, growth_rate, 24)
     X, filters = transition_layer(X, filters, compression)
     X, filters = dense_block(X, filters, growth_rate, 16)
-    X = K.layers.AveragePooling2D(pool_size=7)(X)
+    X = K.layers.AveragePooling2D(pool_size=6)(X)
     output = K.layers.Dense(units=1000, activation='softmax',
                             kernel_initializer=kernel)(X)
-    model = K.models.Model(inputs=img_input, outputs=output)
-    return model
+    return K.models.Model(inputs=X_input, outputs=output)
