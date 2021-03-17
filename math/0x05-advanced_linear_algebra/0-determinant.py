205fa3c61669d412d6fe32f091588317538058bb
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-03-17 12:29:06 +0100

    firsy

---
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""calculates the determinant of a matrix:"""
+
+
+def construire_mat(mat, i):
+    '''construire matrix'''
+    new_mat = []
+    for l in range(1, len(mat)):
+        wiw = []
+        for j in range(len(mat[0])):
+            if j != i:
+                wiw.append(mat[l][j])
+        new_mat.append(wiw)
+
+    return new_mat
+
+
+def determinant(matrix):
+    """calcul determinant matrix"""
+    if not isinstance(matrix, list) or matrix == []:
+        raise TypeError('matrix must be a list of lists')
+
+    if any(not isinstance(row, list) for row in matrix):
+        raise TypeError('matrix must be a list of lists')
+
+    if matrix == [[]]:
+        return 1
+
+    if len(matrix[0]) != len(matrix):
+        raise ValueError('matrix must be a square matrix')
+
+    if len(matrix) == 1:
+        return matrix[0][0]
+
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    det = 0
+    for i, j in enumerate(matrix[0]):
+
+        new_mat = construire_mat(matrix, i)
+        if i % 2 == 1:
+            det += j * (-1) * determinant(new_mat)
+        else:
+            det += j * determinant(new_mat)
+    return det
---
+++ math/0x05-advanced_linear_algebra/README.md
@@ -0,0 +1,8 @@
+<h3>General</h3>
+<ul>
+<li>What is a determinant? How would you calculate it?</li>
+<li>What is a minor, cofactor, adjugate? How would calculate them?</li>
+<li>What is an inverse? How would you calculate it?</li>
+<li>What are eigenvalues and eigenvectors? How would you calculate them?</li>
+<li>What is definiteness of a matrix? How would you determine a matrix’s definiteness?</li>
+</ul>
