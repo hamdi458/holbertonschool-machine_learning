1ce99f39beddb861148865c41889e3a297d85d3b
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-03-18 17:01:56 +0100

    fix none

--- math/0x05-advanced_linear_algebra/5-definiteness.py
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -5,24 +5,20 @@ import numpy as np
 
 def definiteness(matrix):
     """calculates the definiteness of a matrix"""
+
     if not isinstance(matrix, np.ndarray):
         raise TypeError('matrix must be a numpy.ndarray')
 
-    if len(matrix.shape) == 1:
-        return None
-
-    if matrix.shape[0] != matrix.shape[1]:
-        return None
-
     if not np.array_equal(matrix.T, matrix):
         return None
-    if np.all(np.linalg.eig(matrix) > 0):
+
+    if np.all(np.linalg.eigvals(matrix) > 0):
         return("Positive definite")
-    elif np.all(np.linalg.eig(matrix) < 0):
+    elif np.all(np.linalg.eigvals(matrix) < 0):
         return("negative definite")
-    elif np.all(np.linalg.eig(matrix) >= 0):
+    elif np.all(np.linalg.eigvals(matrix) >= 0):
         return("Positive semi-definite")
-    elif np.all(np.linalg.eig(matrix) <= 0):
+    elif np.all(np.linalg.eigvals(matrix) <= 0):
         return("negative semi-definite")
     else:
         return("Indefinite")
