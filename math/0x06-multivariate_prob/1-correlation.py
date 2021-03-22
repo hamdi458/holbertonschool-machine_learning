ba4304b7848a7139de16699300002526b1074678
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-03-22 21:24:04 +0100

    fix

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -8,7 +8,7 @@ def correlation(C):
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
     if len(C.shape) != 2:
-        raise TypeError("C must be a 2D square matrix")
+        raise ValueError("C must be a 2D square matrix")
     if C.shape[1] != C.shape[0]:
         raise ValueError('C must be a 2D square matrix')
     v = np.diag(1 / np.sqrt(np.diag(C)))
