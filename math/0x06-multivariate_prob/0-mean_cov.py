5e267729cc613f0d385bcfd5e0072af2a953804a
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-03-22 19:44:27 +0100

    first

---
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""calculates the mean and covariance of a data set"""
+import numpy as np
+
+
+def mean_cov(X):
+    """calculates the mean and covariance of a data set"""
+    if not isinstance(X, np.ndarray):
+        raise TypeError("X must be a 2D numpy.ndarray")
+    if len(X.shape) != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    if X.shape[0] < 2:
+        raise ValueError("X must contain multiple data points")
+    n = X.shape[0] - 1
+    mean = np.mean(X, axis=0)
+    return mean, np.matmul((X - mean), (X - mean).T)/n
---
+++ math/0x06-multivariate_prob/README.md
@@ -0,0 +1,8 @@
+<ul>
+<li>Who is Carl Friedrich Gauss?</li>
+<li>What is a joint/multivariate distribution?</li>
+<li>What is a covariance?</li>
+<li>What is a correlation coefficient?</li>
+<li>What is a covariance matrix?</li>
+<li>What is a multivariate Gaussian distribution?</li>
+</ul>
