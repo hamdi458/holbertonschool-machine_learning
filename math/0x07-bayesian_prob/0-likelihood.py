da7e02611bf53529905d26d063aef8530474b3df
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-03-25 18:20:45 +0100

    first

---
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+likelihood
+"""
+import numpy as np
+
+
+def likelihood(x, n, P):
+    """Function that calculates the likelihood data """
+
+    if not isinstance(n, int) or (n <= 0):
+        raise ValueError('n must be a positive integer')
+
+    if not isinstance(x, int) or (x < 0):
+        err = 'x must be an integer that is greater than or equal to 0'
+        raise ValueError(err)
+
+    if x > n:
+        raise ValueError('x cannot be greater than n')
+
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        raise TypeError('P must be a 1D numpy.ndarray')
+
+    if np.any(P > 1) or np.any(P < 0):
+        raise ValueError('All values in P must be in the range [0, 1]')
+
+    fa = (np.math.factorial(n))
+    d = (np.math.factorial(x) * np.math.factorial(n - x))
+    fact = fa / d
+    total = factorial * (np.power(P, x)) * (np.power((1 - P), (n - x)))
+
+    return total
---
+++ math/0x07-bayesian_prob/README.md
@@ -0,0 +1,8 @@
+<ul>
+<li>What is Bayesian Probability?</li>
+<li>What is Bayes’ rule and how do you use it?</li>
+<li>What is a base rate?</li>
+<li>What is a prior?</li>
+<li>What is a posterior?</li>
+<li>What is a likelihood?</li>
+</ul>
