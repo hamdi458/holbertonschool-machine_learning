3d12b6f05ef6df66281ff14e37aa78258eecf9bf
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-03-31 19:20:24 +0100

    fix

--- unsupervised_learning/0x01-clustering/2-variance.py
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -5,6 +5,10 @@ import numpy as np
 
 def variance(X, C):
     """alculates the total intra-cluster variance for a data set X"""
+    if (not isinstance(X, np.ndarray) or not isinstance(C, np.ndarray)
+            or len(X.shape) != 2 or len(C.shape) != 2
+            or X.shape[1] != C.shape[1]):
+        return None
     distance = np.sqrt(((X - C[:, np.newaxis])**2).sum(axis=-1))
     min_distance = np.min(distance, axis=0)
     v = np.sum(min_distance ** 2)
