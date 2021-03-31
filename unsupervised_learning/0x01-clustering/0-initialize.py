1d76831664c3a96825bbe10cdf26607778a308b4
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-03-31 19:18:03 +0100

    comm

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -4,9 +4,8 @@ import numpy as np
 
 def initialize(X, k):
     """returns k centroids from the initial points"""
-    if type(X) is not np.ndarray or len(X.shape) != 2:
-        return None
-    if type(k) is not int or k <= 0:
+    if (not isinstance(X, np.ndarray) or not isinstance(k, int) or k <= 0
+            or len(X.shape) != 2):
         return None
     maxi = np.max(X, axis=0)
     mini = np.min(X, axis=0)
