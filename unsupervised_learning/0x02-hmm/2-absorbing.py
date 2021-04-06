92877e25936cc9e757d8a241c75c135511053146
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-04-06 12:14:09 +0100

    wiw

---
+++ unsupervised_learning/0x02-hmm/2-absorbing.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" absorbing chain """
+import numpy as np
+
+
+def absorbing(P):
+    """determines if a markov chain is absorbing"""
+    diag = np.diag(P)
+    if not((diag == 1).any()):
+        return False
+    elif (diag == 1).all():
+        return True
+    chemin_absor = []
+    for i in range(P.shape[0]):
+        for j in range(P.shape[0]):
+            if P[i, j] == 1 and i == j:
+                chemin_absor.append(i)
+
+    for _ in range(P.shape[0]):
+        for i in range(P.shape[0]):
+            for j in range(P.shape[0]):
+                if (P[i, j] > 0 and any(chemin_absor) == j and not(any(chemin_absor) == i)):
+                    chemin_absor.append(i)
+    if len(chemin_absor) < P.shape[0]:
+        return False
+    else:
+        return True
