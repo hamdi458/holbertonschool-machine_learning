ab176a4dd7f62e6e2427c0e27a241005d326c715
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-04-29 08:03:57 +0100

    ltsm

--- supervised_learning/0x0D-RNNs/3-lstm_cell.py
+++ supervised_learning/0x0D-RNNs/3-lstm_cell.py
@@ -4,6 +4,7 @@ import numpy as np
 
 
 class LSTMCell():
+    """class LSTMCell"""
     def __init__(self, i, h, o):
         """initialize class constructor"""
         self.Wf = np.random.normal(size=(i + h, h))
