e470aa4846970f3eeebf107d4024eb0081898de1
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-05-10 19:58:59 +0100

    fix pycodestyle

--- supervised_learning/0x0E-time_series/preprocess_data.py
+++ supervised_learning/0x0E-time_series/preprocess_data.py
@@ -5,22 +5,21 @@ import pandas as pd
 
 def preprocessing(csv_path):
     df = pd.read_csv(csv_path)
-    #drop nan
+    # drop nan
     df = df.dropna()
-    #take the last 2 years data
+    # take the last 2 years data
     df = df[-(730 * 24 * 60):]
-    #encode the date
-    df['Date'] = pd.to_datetime(df['Timestamp'],unit='s')
-    #make date as index
+    # encode the date
+    df['Date'] = pd.to_datetime(df['Timestamp'], unit='s')
+    # make date as index
     df = df.set_index('Date')
-    #you should always split your data into training, validation, testing
-    #(usually 80%, 10%, 10%)
+    # you should always split your data into training, validation, testing
+    # (usually 80%, 10%, 10%)
     df_train = df[:int(len(df)*80/100)]
     df_valid = df[int(len(df_train)):int(len(df)*90/100)]
     df_test = df[int(-(len(df)*10/100)):]
 
-
-    #normalize data
+    # normalize data
     train_mean = df_train.mean()
     train_std = df_train.std()
     x_train = (df_train - train_mean) / train_std
