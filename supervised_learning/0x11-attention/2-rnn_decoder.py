aeb16e7f75cc6b4eca3d3beecb68547cb64c5716
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-05-20 17:23:06 +0100

    fix pycodestyle

--- supervised_learning/0x11-attention/2-rnn_decoder.py
+++ supervised_learning/0x11-attention/2-rnn_decoder.py
@@ -11,16 +11,17 @@ class RNNDecoder(tf.keras.Model):
         super(RNNDecoder, self).__init__()
         self.embedding = tf.keras.layers.Embedding(vocab, embedding)
         self.gru = tf.keras.layers.GRU(units,
-                                    return_sequences=True,
-                                    return_state=True,
-                                    recurrent_initializer='glorot_uniform')
+                                       return_sequences=True,
+                                       return_state=True,
+                                       recurrent_initializer='glorot_uniform')
         self.F = tf.keras.layers.Dense(vocab)
 
         self.attention = SelfAttention(units)
-  
+
     def call(self, x, s_prev, hidden_states):
         """decode for machine translation:"""
-        context_vector, attention_weights = self.attention(s_prev, hidden_states)
+        context_vector, attention_weights = self.attention(s_prev,
+                                                           hidden_states)
         x = self.embedding(x)
         x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
         output, state = self.gru(x)
