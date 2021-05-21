03c0d32ed27d0a9a59cb9cbd4798a7b108b49f32
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-05-21 18:10:25 +0100

    first

---
+++ supervised_learning/0x11-attention/8-transformer_decoder_block.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Transformer Decoder Block"""
+import tensorflow as tf
+MultiHeadAttention = __import__('6-multihead_attention').MultiHeadAttention
+
+
+class DecoderBlock(tf.keras.layers.Layer):
+    """class DecoderBlock"""
+    def __init__(self, dm, h, hidden, drop_rate=0.1):
+        """initializer Class constructor """
+        super().__init__()
+        self.mha1 = MultiHeadAttention(dm, h)
+        self.mha2 = MultiHeadAttention(dm, h)
+        self.dense_hidden = tf.keras.layers.Dense(units=hidden,
+                                                  activation='relu')
+        self.dense_output = tf.keras.layers.Dense(units=dm)
+        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm3 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = tf.keras.layers.Dropout(drop_rate)
+        self.dropout2 = tf.keras.layers.Dropout(drop_rate)
+        self.dropout3 = tf.keras.layers.Dropout(drop_rate)
+
+    def call(self, x, encoder_output, training, look_ahead_mask, padding_mask):
+        """ create an encoder block for a transformer"""
+        att, attn_weights_block1 = self.mha1(x, x, x, look_ahead_mask)
+        att = self.dropout1(att, training=training)
+        output = self.layernorm1(att + x)
+        attn2, attn_weights_block2 = self.mha2(output, encoder_output,
+                                               encoder_output,
+                                               padding_mask)
+        attn2 = self.dropout2(attn2, training=training)
+        out2 = self.layernorm2(attn2 + output)
+        out = self.dense_hidden(out2)
+        out = self.dense_output(out)
+        out = self.dropout3(out, training=training)
+        out3 = self.layernorm3(out + out2)
+        return out3
