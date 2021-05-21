bf5ef07ff92a7ac829b6c6cdde0c266a3ca36b78
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-05-21 22:31:50 +0100

    fix

--- supervised_learning/0x11-attention/1-self_attention.py
+++ supervised_learning/0x11-attention/1-self_attention.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python3
-""" class RNNCell """
+""" self attention class """
 import tensorflow as tf
 
 
 class SelfAttention(tf.keras.layers.Layer):
-    """ the class SelfAttention """
+    """ Self attention class """
     def __init__(self, units):
-        """initializer Class constructor"""
+        """ initialization function """
         super(SelfAttention, self).__init__()
         self.W = tf.keras.layers.Dense(units)
         self.U = tf.keras.layers.Dense(units)
         self.V = tf.keras.layers.Dense(1)
 
     def call(self, s_prev, hidden_states):
-        """calculate the attention for machine translation
-            based on this paper:"""
-        query_with_time_axis = tf.expand_dims(s_prev, 1)
-        score = self.V(tf.nn.tanh(self.W1(query_with_time_axis) +
-                       self.W2(hidden_states)))
-        attention_weights = tf.nn.softmax(score, axis=1)
-        context_vector = attention_weights * hidden_states
-        context_vector = tf.reduce_sum(context_vector, axis=1)
-        return context_vector, attention_weights
+        """ Function that returns context and weights"""
+        ss_prev = tf.expand_dims(s_prev, axis=1)
+        score_t = self.V(tf.nn.tanh(self.W(ss_prev) + self.U(hidden_states)))
+        weights = tf.nn.softmax(score_t, axis=1)
+        context = weights * hidden_states
+        context = tf.reduce_sum(context, axis=1)
+        return context, weights
