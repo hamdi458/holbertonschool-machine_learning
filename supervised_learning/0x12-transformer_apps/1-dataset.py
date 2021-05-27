b5dc6744ddcc54feb1696aafe03f308a642c12fc
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-05-27 18:34:40 +0100

    first

--- supervised_learning/0x12-transformer_apps/1-dataset.py
+++ supervised_learning/0x12-transformer_apps/1-dataset.py
@@ -12,20 +12,25 @@ class Dataset:
                                     split='train', as_supervised=True)
         self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
                                     split='validation', as_supervised=True)
-
         a, b = self.tokenize_dataset(self.data_train)
-        self.tokenizer_en = a
-        self.tokenizer_pt = b
+        self.tokenizer_en = b
+        self.tokenizer_pt = a
 
     def tokenize_dataset(self, data):
         """creates sub-word tokenizers for our dataset"""
         pp = []
         ee = []
         for pt, en in data:
-            pp += (pt.numpy()).split()
-            ee += (en.numpy()).split()
-        encoder = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+            pp.append(pt.numpy())
+            ee.append(en.numpy())
+        tokenizer_pt = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
             pp, target_vocab_size=2**15)
-        encoder2 = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+        tokenizer_en = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
             ee, target_vocab_size=2**15)
-        return encoder, encoder2
+        return tokenizer_pt, tokenizer_en
+
+    def encode(self, pt, en):
+        """encode function"""
+        tok_pt = [self.tokenizer_pt.vocab_size] + self.tokenizer_pt.encode(pt.numpy()) + [(self.tokenizer_pt.vocab_size) + 1]
+        tok_en = [self.tokenizer_en.vocab_size] + self.tokenizer_en.encode(en.numpy()) + [(self.tokenizer_en.vocab_size) + 1]
+        return tok_pt, tok_en
---
+++ supervised_learning/0x12-transformer_apps/2-dataset.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""Dataset"""
+import tensorflow.compat.v2 as tf
+import tensorflow_datasets as tfds
+
+
+class Dataset:
+    """Dataset class"""
+    def __init__(self):
+        """initialize class constructor"""
+        self.data_train = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='train', as_supervised=True)
+        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='validation', as_supervised=True)
+        a, b = self.tokenize_dataset(self.data_train)
+        self.tokenizer_en = b
+        self.tokenizer_pt = a
+        self.data_train = self.data_train.map(self.tf_encode)
+        self.data_valid = self.data_valid.map(self.tf_encode)
+
+    def tokenize_dataset(self, data):
+        """creates sub-word tokenizers for our dataset"""
+        pp = []
+        ee = []
+        for pt, en in data:
+            pp.append(pt.numpy())
+            ee.append(en.numpy())
+        tokenizer_pt = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+            pp, target_vocab_size=2**15)
+        tokenizer_en = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+            ee, target_vocab_size=2**15)
+        return tokenizer_pt, tokenizer_en
+
+    def encode(self, pt, en):
+        """encode"""
+        tok_pt = [self.tokenizer_pt.vocab_size]+self.tokenizer_pt.encode(pt.numpy())+[(self.tokenizer_pt.vocab_size) + 1]
+        tok_en = [self.tokenizer_en.vocab_size]+self.tokenizer_en.encode(en.numpy())+[(self.tokenizer_en.vocab_size) + 1]
+        return tok_pt, tok_en
+
+    def tf_encode(self, pt, en):
+        """tf_encode"""
+        ptt, enn = tf.py_function(func=self.encode,
+                                  inp=[pt, en],
+                                  Tout=[tf.int64,
+                                  tf.int64],
+                                  name=None)
+        ptt.set_shape[None]
+        enn.set_shape[None]
+        return ptt, enn
+        
