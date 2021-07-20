e817bdd102b24200e0256e3b35bb367c915fa740
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-07-20 23:27:57 +0100

    remove index

--- pipeline/0-from_numpy.py
+++ pipeline/0-from_numpy.py
@@ -9,14 +9,9 @@ import string
 def from_numpy(array):
     """ creates a pd.DataFrame from a np.ndarray"""
     index_values = list(string.ascii_lowercase)
-    comumn_number = array.shape[0]
-    column_values = []
-    for i in range(comumn_number):
-        column_values.append(i)
     index = []
     for i in range(array.shape[1]):
         index.append(index_values[i])
     df = pd.DataFrame(data=array,
-                      index=column_values,
                       columns=index)
     return df
