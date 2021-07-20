483025d6b77e320e8bb9d484772151ac0e80c5d5
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-07-20 23:19:41 +0100

    first

---
+++ pipeline/0-from_numpy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import pandas as pd
+import string
+
+
+def from_numpy(array):
+    index_values = list(string.ascii_lowercase)
+    comumn_number = array.shape[0]
+    column_values = []
+    for i in range(comumn_number):
+        column_values.append(i)
+    index = []
+    for i in range(array.shape[1]):
+        index.append(index_values[i])
+    df = pd.DataFrame(data=array,
+                      index=column_values,
+                      columns=index)
+    return df
