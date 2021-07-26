a3c6578f357dbe834346a6ba5426c6a335c2a018
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-07-26 12:31:25 +0100

    fix

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -8,13 +8,14 @@ import sys
 
 
 if __name__ == '__main__':
-    r = requests.get(sys.argv[1])
-    data = r.json()
-    if r .status_code == 404:
+    url = sys.argv[1]
+    req = requests.get(url)
+    data = req.json()
+    if req .status_code == 404:
         print("Not found")
-    elif r .status_code == 200:
+    elif req .status_code == 200:
         print(data["location"])
-    elif r .status_code == 403:
+    elif req .status_code == 403:
         limit = req.headers["X-Ratelimit-Reset"]
         x = (int(limit) - int(time.time())) / 60
         print("Reset in {} min".format(int(x)))
