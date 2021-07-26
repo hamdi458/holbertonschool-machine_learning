041adda8676a0cef0a6717a6b0900247965ce975
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-07-26 12:27:17 +0100

    fix

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -12,12 +12,10 @@ if __name__ == '__main__':
     if r.status_code == 200:
         data = r.json()
         print(data['location'])
-        exit()
 
     elif r.status_code == 403:
         tim = data.headers['X-Ratelimit-Reset']
         tim = int(tim) - int(time.time())
         print("Reset in {} min".format(int(tim / 60)))
-        exit()
-
-    print('Not found')
+    else:
+        print('Not found')
