dae4c83ef0b38f2fa11b3a03aa05eaa1d2b86b0c
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-08-02 12:27:14 +0100

    first

---
+++ pipeline/0x02-databases/20-average_score.sql
@@ -0,0 +1,14 @@
+-- creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
+
+DELIMITER $$
+
+CREATE OR REPLACE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+BEGIN
+
+SELECT AVG(score) INTO @sc FROM corrections WHERE corrections.user_id = user_id;
+
+UPDATE users
+SET average_score = @sc
+WHERE id = user_id;
+END $$
+DELIMITER ;
