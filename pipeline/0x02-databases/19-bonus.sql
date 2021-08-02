9b2b153e78a11f1fdab745d57846197f66933592
hamdi458 <hamdi.ghorbel645@gmail.com>
2021-08-02 11:53:28 +0100

    first

--- pipeline/0x02-databases/19-bonus.sql
+++ pipeline/0x02-databases/19-bonus.sql
@@ -4,13 +4,14 @@ DELIMITER $$
 
 CREATE OR REPLACE PROCEDURE AddBonus (IN user_id INT,
 IN project_name VARCHAR(255),
-IN score INT
-)
+IN score INT)
 BEGIN
 IF NOT EXISTS (SELECT id FROM projects WHERE name = project_name) THEN
     INSERT INTO projects(name) VALUES (project_name);
 END IF;
-SET @id_of_project = (SELECT id FROM projects WHERE name = project_name);   
+SELECT id INTO @id_of_project FROM projects WHERE name = project_name;
+-- SET @id_of_project = (SELECT id FROM projects WHERE name = project_name);   
+
 INSERT INTO corrections(user_id, project_id, score)
             VALUES(user_id, @id_of_project, score);
 END $$
