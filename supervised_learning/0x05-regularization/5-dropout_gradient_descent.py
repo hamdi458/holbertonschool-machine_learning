4a09a9c0f4919188d2be500e0ffcfc26a69c2819
hamdi458 <hamdi.ghorbel465@gmail.com>
2022-05-26 16:32:26 +0100

    fix

--- supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
+++ supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
@@ -8,17 +8,25 @@ import numpy as np
 
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """updates the weights of a neural network with Dropout"""
-    wht = weights.copy()
     m = Y.shape[1]
-    dz = cache["A"+str(L)] - Y
-    for i in range(L, 0, -1):
-        dw = 1 / m * np.dot(dz, cache["A"+str(i-1)].T)
-        db = 1 / m * np.sum(dz, axis=1, keepdims=True)
-        weights["W"+str(i)] = weights["W"+str(i)]-(dw*alpha)
-        weights["b"+str(i)] = weights["b"+str(i)]-(db*alpha)
-        w = (cache["A"+str(i-1)] - cache["A"+str(i-1)] ** 2)
-        qqq = (1 - np.power(cache["A"+str(i-1)], 2))
-        if i > 1:
-            a = cache['D' + str(i-1)] / keep_prob
-            dz = np.dot(wht['W' + str(i)].T, dz) * qqq * a
+    weights_copy = weights.copy()
 
+    for layer_index in range(L, 0, -1):
+        A = cache["A" + str(layer_index)]
+
+        if layer_index == L:
+            dz = A - Y
+        if layer_index>1:
+            dz = np.multiply(
+                np.dot(weights_copy["W" + str(layer_index + 1)].T, dz),
+                (1 - np.power(A, 2)),
+            )
+            dz = (dz * cache["D" + str(layer_index)]) / keep_prob
+
+        dw = 1 / m * np.dot(dz, cache["A" + str(layer_index - 1)].T)
+        db = 1 / m * np.sum(dz, axis=1, keepdims=True)
+
+        weights["W" + str(layer_index)] = weights[
+            "W" + str(layer_index)] - (alpha * dw)
+        weights["b" + str(layer_index)] = weights[
+            "b" + str(layer_index)] - (alpha * db)
