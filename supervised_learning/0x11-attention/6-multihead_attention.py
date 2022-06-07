6bbc3426556f36cfa611d1bdf889cb562185d932
hamdi458 <hamdi.ghorbel465@gmail.com>
2022-06-07 11:34:38 +0100

    try

--- supervised_learning/0x11-attention/6-multihead_attention.py
+++ supervised_learning/0x11-attention/6-multihead_attention.py
@@ -53,7 +53,7 @@ class MultiHeadAttention(tf.keras.layers.Layer):
             # Then, swap the heads & tokens axes
             attention_parameters[i] = tf.transpose(
                 tf.reshape(
-                    parameter, (*parameter.shape[:-1], self.h, self.depth)
+                    parameter, (batch_size, -1,self.h, self.depth)
                 ),
                 perm=[0, 2, 1, 3]
             )
@@ -63,7 +63,7 @@ class MultiHeadAttention(tf.keras.layers.Layer):
         attention_scores = tf.transpose(attention_scores, perm=[0, 2, 1, 3])
         # And merge the heads back into a single features axis
         attention_scores = tf.reshape(
-            attention_scores, (batch_size,-1, self.dm))
+            attention_scores, (batch_size, -1, self.dm))
         output = self.linear(attention_scores)
 
         return output, weights
