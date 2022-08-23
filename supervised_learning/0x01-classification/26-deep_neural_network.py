be65852696ba3216995f94ee94d51ee842653b47
hamdi458 <hamdi.ghorbel465@gmail.com>
2022-08-23 15:31:16 +0100

    verif code

--- supervised_learning/0x01-classification/26-deep_neural_network.py
+++ supervised_learning/0x01-classification/26-deep_neural_network.py
@@ -1,118 +1,132 @@
 #!/usr/bin/env python3
-"""defines a neural network with one hidden layer"""
+"""
+Module for class DeepNeuralNetwork
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
 
 class DeepNeuralNetwork:
-    """class NeuralNetwork"""
+    """
+    Defines a deep neural network performing
+    binary classification
+    """
+
     def __init__(self, nx, layers):
-        if type(nx) is not int:
+        """
+        Class Constructor
+        nx is the number of input features
+        layers is a list representing the number of
+            nodes in each layer of the network
+        """
+        if type(nx) != int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if type(layers) is not list:
+        if type(layers) != list or layers == []:
             raise TypeError("layers must be a list of positive integers")
-        if not all(map(lambda x: x > 0 and isinstance(x, int), layers)):
-            raise TypeError('layers must be a list of positive integers')
-        if len(layers) == 0:
-            raise TypeError('layers must be a list of positive integers')
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
-        self.weights['b' + str(1)] = np.zeros((layers[0], 1))
-        self.weights['W' + str(1)] = np.random.normal(size=(layers[0],
-                                                      nx))*np.sqrt(2/nx)
-        for i in range(1, len(layers)):
-            self.weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
-            a = np.sqrt(2/layers[i-1])
-            la = np.random.normal(size=(layers[i], layers[i-1]))*a
-            self.weights['W' + str(i + 1)] = la
+        for layer in range(self.__L):
+            if layers[layer] < 1:
+                raise TypeError("layers must be a list of positive integers")
+            if layer == 0:
+                layer_prev = nx
+            else:
+                layer_prev = layers[layer - 1]
+            self.__weights['W' + str(layer + 1)] = np.random.randn(
+                layers[layer], layer_prev) * np.sqrt(2 / layer_prev)
+            self.__weights['b' + str(layer + 1)] = np.zeros((layers[layer], 1))
 
     @property
     def L(self):
-        """The number of layers in the neural network"""
+        """ Getter for L """
         return self.__L
 
     @property
     def cache(self):
-        """dictionary to hold all intermediary values of the network"""
+        """ Getter for cache """
         return self.__cache
 
     @property
     def weights(self):
-        """dictionary to hold all weights and biased of the network."""
+        """ Getter for weights """
         return self.__weights
 
     def forward_prop(self, X):
-        """Calculates the forward propagation of the neural network"""
-        self.cache['A0'] = X
-        for i in range(self.L):
-            b = self.__weights["b" + str(i+1)]
-            z = np.dot(self.weights['W'+str(i+1)], self.cache["A"+str(i)])+b
-            self.cache["A"+str(i+1)] = 1/(1+np.exp(-z))
-        return self.cache["A"+str(self.L)], self.cache
+        """ Calculates the forward propagation of the neural network """
+        self.__cache["A0"] = X
+        for layer in range(self.__L):
+            Z = np.matmul(
+                self.__weights["W" + str(layer + 1)],
+                self.__cache["A" + str(layer)]) + \
+                self.__weights["b" + str(layer + 1)]
+            self.__cache["A" + str(layer + 1)] = 1 / (1 + np.exp(-Z))
+        return self.__cache["A" + str(self.__L)], self.__cache
 
     def cost(self, Y, A):
-        """Calculates the cost of the model using logistic regression"""
-        m = Y.shape[1]
-        q = - (1 / m)
-        e = np.multiply(1 - Y, np.log(1.0000001 - A))
-        cost = q * np.sum(np.multiply(Y, np.log(A)) + e)
+        """ Calculates the cost of the model using logistic regression """
+        m = -1 / Y.shape[1]
+        cost = m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
         return cost
 
     def evaluate(self, X, Y):
-        """Evaluates the neural network’s predictions"""
-        c, c1 = self.forward_prop(X)
-        pred = np.round(c)
-        return pred.astype(np.int), self.cost(Y, c)
+        """ Evaluates the neural network’s predictions """
+        A = self.forward_prop(X)[0]
+        pred = np.where(A >= 0.5, 1, 0)
+        cost = self.cost(Y, A)
+        return pred, cost
 
     def gradient_descent(self, Y, cache, alpha=0.05):
-        """gradient descent"""
-        wht = self.__weights.copy()
-        m = Y.shape[1]
-        dz = cache["A"+str(self.L)] - Y
-        for i in range(self.L, 0, -1):
-            dw = 1 / m * np.dot(dz, self.__cache["A"+str(i-1)].T)
-            db = 1 / m * np.sum(dz, axis=1, keepdims=True)
-            self.__weights["W"+str(i)] = self.__weights["W"+str(i)]-(dw*alpha)
-            self.__weights["b"+str(i)] = self.__weights["b"+str(i)]-(db*alpha)
-            w = (self.cache["A"+str(i-1)] - self.cache["A"+str(i-1)] ** 2)
-            dz = np.dot(wht["W"+str(i)].T, dz) * w
-        return self.__weights
+        """ Calculates one pass of gradient descent on the neuron """
+        m = 1 / Y.shape[1]
+        for i in reversed(range(1, self.__L + 1)):
+            A_prev = self.__cache["A" + str(i - 1)]
+            A = self.__cache["A" + str(i)]
+            W = self.__weights["W" + str(i)]
+            if i == self.__L:
+                dz = A - Y
+            else:
+                dz = da * (A * (1 - A))
+            dw = m * np.matmul(dz, A_prev.T)
+            db = m * np.sum(dz, axis=1, keepdims=True)
+            da = np.matmul(W.T, dz)
+            self.__weights["W" + str(i)] -= (alpha * dw)
+            self.__weights["b" + str(i)] -= (alpha * db)
 
     def train(self, X, Y, iterations=5000, alpha=0.05, verbose=True,
               graph=True, step=100):
-        """evaluation of the training data after iterations"""
-        if type(iterations) is not int:
+        """ Trains the deep neural network """
+        if type(iterations) != int:
             raise TypeError("iterations must be an integer")
-        if iterations < 1:
+        if iterations <= 0:
             raise ValueError("iterations must be a positive integer")
-        if type(alpha) is not float:
+        if type(alpha) != float:
             raise TypeError("alpha must be a float")
         if alpha <= 0:
             raise ValueError("alpha must be positive")
         if verbose or graph:
-            if type(step) is not int:
+            if type(step) != int:
                 raise TypeError("step must be an integer")
-            if step < 0 or step > iterations:
+            if step <= 0 or step >= iterations:
                 raise ValueError("step must be positive and <= iterations")
-        arrX = []
-        arrY = []
-        for it in range(iterations):
+        costs = []
+        step_array = list(range(0, iterations + 1, step))
+        for i in range(iterations + 1):
             self.forward_prop(X)
-            self.gradient_descent(Y, self.cache, alpha)
-            j = self.cost(Y, self.cache["A"+str(self.L)])
-            if verbose:
-                print("Cost after {} iterations: {}".format(it, j))
-                arrX.append(it + step)
-                arrY.append(j)
+            self.gradient_descent(Y, self.__cache, alpha)
+            cost = self.cost(Y, self.__cache['A' + str(self.__L)])
+            if verbose and (i % step == 0 or i == iterations):
+                costs.append(cost)
+                print("Cost after {} iterations: {}".format(i, cost))
+
         if graph:
-            plt.title('Training Cost')
+            plt.plot(step_array, costs)
             plt.xlabel('iteration')
             plt.ylabel('cost')
-            plt.plot(arrX, arrY)
+            plt.title('Training Cost')
             plt.show()
         return self.evaluate(X, Y)
 
