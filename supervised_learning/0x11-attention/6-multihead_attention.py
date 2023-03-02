17c611457558f78928328bcf9b63e1dd2c769adb
hamdi458 <56004494+hamdi458@users.noreply.github.com>
2023-03-02 15:40:28 +0100

    Update 6-multihead_attention.py

--- supervised_learning/0x11-attention/6-multihead_attention.py
+++ supervised_learning/0x11-attention/6-multihead_attention.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
-""" Defines `MultiHeadAttention`. """
+"""
+based on:
+https://www.tensorflow.org/tutorials/text/transformer#multi-head_attention
+Multihead Attention
+"""
+
 import tensorflow as tf
 sdp_attention = __import__('5-sdp_attention').sdp_attention
 
 
 class MultiHeadAttention(tf.keras.layers.Layer):
-    """ A multi-head attention layer. """
-
+    """
+    Multi Head Attention Class
+    """
     def __init__(self, dm, h):
-        """
-        Initializes a MultiHeadAttention layer.
-        dm: An integer divisible by h representing the dimensionality of the
-            model.
-        h: An integer representing the number of heads.
-        """
+        """ Function that initializes """
         super().__init__()
         self.h = h
         self.dm = dm
@@ -23,46 +24,24 @@ class MultiHeadAttention(tf.keras.layers.Layer):
         self.Wv = tf.keras.layers.Dense(dm)
         self.linear = tf.keras.layers.Dense(dm)
 
+    def split(self, x, batch_size):
+        """ Function that """
+        x = tf.reshape(x, (batch_size, -1, self.h, self.depth))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
     def call(self, Q, K, V, mask):
-        """
-        Executes the MultiHeadAttention layer.
-        Q: A tensor of shape (batch, seq_len_q, dk) containing the input to
-            generate the query matrix.
-            dk: The number of feature dimensions in `K`.
-        K: A tensor of shape (batch, seq_len_v, dk) containing the input to
-            generate the key matrix.
-        V: A tensor of shape (batch, seq_len_v, dv) containing the input to
-            generate the value matrix.
-            dv: The number of feature dimensions in `V`.
-        mask: Always None.
-        Returns: (output, weights)
-            output: A tensor with its last two dimensions as (..., seq_len_q,
-                dm) containing the scaled dot product attention.
-            weights: A tensor with its last three dimensions as (..., h,
-                seq_len_q, seq_len_v) containing the attention weights.
-        """
-        attention_parameters = [
-            self.Wq(Q),
-            self.Wk(K),
-            self.Wv(V)
-        ]
-        for i, parameter in enumerate(attention_parameters):
-            # Split the feature axis into heads x depth, where depth is a
-            # subset/slice of the features
-            # Then, swap the heads & tokens axes
-            attention_parameters[i] = tf.transpose(
-                tf.reshape(
-                    parameter, (*parameter.shape[:-1], self.h, self.depth)
-                ),
-                perm=[0, 2, 1, 3]
-            )
-
-        attention_scores, weights = sdp_attention(attention_parameters[0],attention_parameters[1],attention_parameters[2], mask)
-        # Un-swap the heads & tokens axes
-        attention_scores = tf.transpose(attention_scores, perm=[0, 2, 1, 3])
-        # And merge the heads back into a single features axis
-        attention_scores = tf.reshape(
-            attention_scores, (*attention_scores.shape[:-2], self.dm))
-        output = self.linear(attention_scores)
-
-        return output, weights
+        """ Function that returns output, weights """
+        batch_size = tf.shape(Q)[0]
+        q = self.Wq(Q)
+        k = self.Wk(K)
+        v = self.Wv(V)
+        q = self.split(q, batch_size)
+        k = self.split(k, batch_size)
+        v = self.split(v, batch_size)
+        sc_attention, attention_weights = sdp_attention(q, k, v, mask)
+        sc_attention = tf.transpose(sc_attention,
+                                    perm=[0, 2, 1, 3])
+        concat_attention = tf.reshape(sc_attention,
+                                      (batch_size, -1, self.dm))
+        output = self.linear(concat_attention)
+        return output, attention_weights
